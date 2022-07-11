4a3695696dba52419561545eda27886743a77b4a
Joey Lim <jyorin0206@gmail.com>
2022-07-11 16:44:02 +0800

    added expenditure tab

--- app/src/main/java/com/example/flux_compose/main/home/total_expenses/TotalExpensesScreen.kt
+++ app/src/main/java/com/example/flux_compose/main/home/total_expenses/TotalExpensesScreen.kt
@@ -2,19 +2,25 @@ package com.example.flux_compose.main.home.total_expenses
 
 import android.os.Build
 import androidx.annotation.RequiresApi
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Button
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.flux_compose.R
 import com.example.flux_compose.composables.CircularBudgetProgress
 import com.example.flux_compose.composables.CustomAppBar
 import com.example.flux_compose.composables.CustomCalendar
+import com.example.flux_compose.composables.EntryListItem
+import com.example.flux_compose.main.Entry
 import com.example.flux_compose.utils.getDaysOfCurrentWeek
 import java.time.LocalDate
 
@@ -29,12 +35,68 @@ fun TotalExpensesScreen(navController: NavController) {
     val year = date.year
     val maxValue = 1800f
     val progress = remember { mutableStateOf(1200f) }
-    Column(modifier = Modifier
-        .padding(horizontal = 24.dp)
-        .fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-        CustomAppBar(screenName = "Total Expenses", menuIcon = R.drawable.ic_baseline_keyboard_backspace_24, onMenuItemClick = { navController.popBackStack() })
-        CustomCalendar(days = getDaysOfCurrentWeek(), currentDay = day.toString(), month = month.toString(), year = year.toString())
+
+    val tabIndex = remember { mutableStateOf(0) }
+    val tabTitles = listOf("Expenditure", "Categories")
+
+    val entriesList = listOf(
+        Entry(name = "Food", date = LocalDate.now(), paymentMethod = "Google Pay", price = "$200", icon = {
+            Icon(
+                painter = painterResource(
+                    id = R.drawable.fastfood
+                ), "",
+                tint = MaterialTheme.colors.primaryVariant
+            )
+        }),
+        Entry(name = "Food", date = LocalDate.now(), paymentMethod = "Google Pay", price = "$200", icon = {
+            Icon(
+                painter = painterResource(
+                    id = R.drawable.fastfood
+                ), "",
+                tint = MaterialTheme.colors.primaryVariant
+            )
+        }),
+        Entry(name = "Food", date = LocalDate.now(), paymentMethod = "Google Pay", price = "$200", icon = {
+            Icon(
+                painter = painterResource(
+                    id = R.drawable.fastfood
+                ), "",
+                tint = MaterialTheme.colors.primaryVariant
+            )
+        }),
+    )
+    Column {
+        Column(modifier = Modifier
+            .padding(horizontal = 24.dp)
+            .fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+            CustomAppBar(screenName = "Total Expenses", menuIcon = R.drawable.ic_baseline_keyboard_backspace_24, onMenuItemClick = { navController.popBackStack() })
+            CustomCalendar(days = getDaysOfCurrentWeek(), currentDay = day.toString(), month = month.toString(), year = year.toString())
+            Spacer(modifier = Modifier.height(30.dp))
+            CircularBudgetProgress(max = maxValue, progress = progress.value)
+
+        }
         Spacer(modifier = Modifier.height(30.dp))
-        CircularBudgetProgress(max = maxValue, progress = progress.value)
+        TabRow(selectedTabIndex = tabIndex.value, backgroundColor = Color.Transparent) {
+            tabTitles.forEachIndexed { index, s ->
+                Tab(selected = tabIndex.value == index, onClick = {
+                    tabIndex.value = index
+
+                }, text = {Text(s)})
+            }
+        }
+        when (tabIndex.value) {
+            0 -> {
+                LazyColumn(content = {
+                    items(entriesList.size) {
+                        EntryListItem(entry = entriesList[it])
+                        Divider(Modifier.padding(vertical = 10.dp))
+                    }
+                }, modifier = Modifier.padding(12.dp))
+            }
+            1 -> {
+                Text("helldo")
+            }
+        }
     }
+
 }
